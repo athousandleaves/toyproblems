c523b0cabf809c5ac9c9327f34d1c6e5284efa83
athousandleaves <superamare@gmail.com>
2018-11-14 10:08:18 -0600

    Reverse linked list in constant space, linear time

--- Reverse_Linked_List.js
+++ Reverse_Linked_List.js
@@ -1,22 +1,36 @@
 // O(n) time, O(n) space
+// var reverseList = function(head) {
+//   if (!head) return [];
+//   const stck = [];
+//   const dump = node => {
+//     while (node) {
+//       stck.push(node.val);
+//       node = node.next;
+//     }
+//   };
+//   dump(head);
+//   const reconstruct = arr => {
+//     const headNode = new ListNode(arr.pop());
+//     let node = headNode;
+//     while (arr.length) {
+//       node.next = new ListNode(arr.pop());
+//       node = node.next;
+//     }
+//     return headNode;
+//   };
+//   return reconstruct(stck);
+// };
+
+// O(1) space, O(n) time
 var reverseList = function(head) {
   if (!head) return [];
-  const stck = [];
-  const dump = node => {
-    while (node) {
-      stck.push(node.val);
-      node = node.next;
-    }
-  };
-  dump(head);
-  const reconstruct = arr => {
-    const headNode = new ListNode(arr.pop());
-    let node = headNode;
-    while (arr.length) {
-      node.next = new ListNode(arr.pop());
-      node = node.next;
-    }
-    return headNode;
-  };
-  return reconstruct(stck);
+  let node = head;
+  let prev = null;
+  while (node) {
+    let tmp = node.next;
+    node.next = prev;
+    prev = node;
+    node = tmp;
+  }
+  return prev;
 };
